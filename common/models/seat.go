42de38606cb72cc5fe86433921bc193fb01b3ea5
radiate-positivity <limon.meow@mail.ru>
2026-05-28 13:35:52 +0400

    Add common modules

--- common/models/seat.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Seat struct {
-	ID          int  `json:"id"`
-	Row         int  `json:"row"`
-	Number      int  `json:"number"`
-	HallID      int  `json:"hallId"`
-	IsAvailable bool `json:"isAvailable"`
-}
